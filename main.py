da26b08ca2d3c6a1c2221f5c48b91fe76b1c24c3
Zanark <kiodeba11@gmail.com>
2019-06-11 19:20:19 +0530

    added gitignore

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from PIL import Image
 from gtts import gTTS
 import os
 
-img = pyss.grab(bbox=(0,600,1919,1060))
-img.save('ss.png')
-dialogue = pyt.image_to_string(img)
+# img = pyss.grab(bbox=(0,750,1919,1060))
+# img.save('ss.png')
+
+img = Image.open('/home/zanark/ss.png').convert('LA')
+
+
+dialogue = pyt.image_to_string(img, lang='eng', config='--psm 9')
 print(dialogue)
 
 tts = gTTS(dialogue, lang='en')      #gTTS at work
 tts.save('dOP.mp3')                #saving as mp3
 os.system('mpg123 dOP.mp3')
-os.system('rm dOP.mp3 ss.png')
+#os.system('rm dOP.mp3 ss.png')
