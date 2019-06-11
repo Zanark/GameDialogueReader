5351ba83335a5719d3ba7d1b8948327922214daa
Zanark <kiodeba11@gmail.com>
2019-06-11 15:21:24 +0530

    added tts

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import pyscreenshot as pyss
+import pytesseract as pyt
+from PIL import Image
+from gtts import gTTS
+import os
 
-im = pyss.grab(bbox=(0,820,1919,1062))
-im.save('ss.png')
+img = pyss.grab(bbox=(0,600,1919,1060))
+img.save('ss.png')
+dialogue = pyt.image_to_string(img)
+print(dialogue)
 
+tts = gTTS(dialogue, lang='en')      #gTTS at work
+tts.save('dOP.mp3')                #saving as mp3
+os.system('mpg123 dOP.mp3')
+os.system('rm dOP.mp3 ss.png')
