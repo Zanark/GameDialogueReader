400934a9e4bf266afba92a3f2a6d415447eb4564
Mohit Agarwal <Mohitagarwal9542@gmail.com>
2019-07-05 01:59:59 +0530

    enhanced image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pyscreenshot as pyss
 import pytesseract as pyt
-from PIL import Image
+from PIL import Image, ImageEnhance
 import PIL.ImageOps
 from gtts import gTTS
 import os
@@ -9,6 +9,8 @@ img = pyss.grab(bbox=(5,780,1910,1060))
 img.save('ss.png')
 img = Image.open('ss.png').convert('L')
 img=PIL.ImageOps.invert(img)
+img = ImageEnhance.Contrast(img).enhance(2.0)
+img = ImageEnhance.Sharpness(img).enhance(2.0)
 img.save("ss.jpg", dpi=(10000,10000))
 img = Image.open('ss.jpg')
 
